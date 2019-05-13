ea6229adb086e0c3ee722f42ba06a67dfc32d9f9
DESKTOP-5TV9LF0\cao zhe <zcao1@luc.edu>
2019-05-13 12:41:05 -0500

    new project

--- app.js
+++ app.js
@@ -15,9 +15,13 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-    
-// mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb://colt:rusty@ds055525.mongolab.com:55525/yelpcamp");
+
+
+const PORT = process.env.PORT || 3000;
+ 
+
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -50,6 +54,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+app.listen(PORT, function(){
+   console.log("Server is listening on port ${PORT}");
 });
+
